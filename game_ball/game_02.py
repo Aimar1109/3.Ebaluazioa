8993237b80f8820492e845e0137bb685c31105b5
Aimar1109 <aimar.junagore@gmail.com>
2024-04-09 12:18:59 +0200

    Reordenacion y crear carpeta y juego ball

---
+++ game_ball/game_02.py
@@ -0,0 +1,38 @@
+import pygame
+
+pygame.init()
+
+ventana = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Game 2")
+
+ball = pygame.image.load("ball.png")
+
+ballrect = ball.get_rect()
+
+speed = [4, 4]
+
+ballrect.move_ip(0, 0)
+
+jugando = True
+
+while jugando:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            jugando = False
+    
+    ballrect = ballrect.move(speed)
+
+    if ballrect.left < 0 or ballrect.right > ventana.get_width():
+        speed[0] = -speed[0]
+    
+    if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
+        speed[1] = -speed[1]
+    
+
+    ventana.fill((252, 243, 207))
+
+    ventana.blit(ball, ballrect)
+    pygame.display.flip()
+    pygame.time.Clock().tick(60)
+
+pygame.quit()
