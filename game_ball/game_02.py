b3bacaf738a5eb7efd201f41354c3abf32fb7675
Aimar1109 <aimar.junagore@gmail.com>
2024-04-17 09:04:24 +0200

    initial menu

--- game_ball/game_02.py
+++ game_ball/game_02.py
@@ -8,7 +8,6 @@ pygame.display.set_caption("Game 2")
 ball = pygame.image.load("ball.png")
 ballrect = ball.get_rect()
 
-speed = [4, 4]
 
 ballrect.move_ip(0, 0)
 
@@ -17,38 +16,71 @@ baterect = bate.get_rect()
 
 baterect.move_ip(240, 450)
 
-jugando = True
+# INITIAL VARIABLES
+running = True
+game_state = False
+speed = [4, 4]
 
-while jugando:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            jugando = False
-    
-    keys = pygame.key.get_pressed()
+def draw_menu():
+    ventana.fill((0, 0, 0))
+    font = pygame.font.SysFont('arial', 40)
+    font1 = pygame.font.SysFont('arial', 60)
 
-    if keys[pygame.K_LEFT]:
-        if not baterect.left < 0:
-            baterect = baterect.move(-3, 0)
-    if keys[pygame.K_RIGHT]:
-        if  baterect.right < ventana.get_width():
-            baterect = baterect.move(3, 0)
-    
-    ballrect = ballrect.move(speed)
+    title1 = font1.render('BALL GAME', True, (255, 255, 255))
+    ventana.blit(title1, (325 - title1.get_width()/2, 150 - title1.get_height()/2))
 
-    if ballrect.left < 0 or ballrect.right > ventana.get_width():
-        speed[0] = -speed[0]
-    
-    if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
-        speed[1] = -speed[1]
-        
-    if baterect.colliderect(ballrect):
-        speed[1] = -speed[1]
+    start_button = font.render('Play', True, (255, 255, 255))
+    start_button_rect = start_button.get_rect(center=(325, 300))
+    ventana.blit(start_button, (325 - start_button.get_width()/2, 250 + start_button.get_height()/2))
 
-    ventana.fill((252, 243, 207))
+    end_button = font.render('Quit', True, (255, 255, 255))
+    end_button_rect = end_button.get_rect(center=(325, 400))
+    ventana.blit(end_button, (325 - end_button.get_width()/2, 350 + end_button.get_height()/2))
 
-    ventana.blit(ball, ballrect)
-    ventana.blit(bate, baterect)
-    pygame.display.flip()
-    pygame.time.Clock().tick(60)
+    pygame.display.update()
+
+    return start_button_rect, end_button_rect
+
+while running:
+    if game_state:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            if not baterect.left < 0:
+                baterect = baterect.move(-3, 0)
+        if keys[pygame.K_RIGHT]:
+            if  baterect.right < ventana.get_width():
+                baterect = baterect.move(3, 0)
+
+        ballrect = ballrect.move(speed)
+
+        if ballrect.left < 0 or ballrect.right > ventana.get_width():
+            speed[0] = -speed[0]
+
+        if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
+            speed[1] = -speed[1]
+
+        if baterect.colliderect(ballrect):
+            speed[1] = -speed[1]
+
+        ventana.fill((252, 243, 207))
+
+        ventana.blit(ball, ballrect)
+        ventana.blit(bate, baterect)
+        pygame.display.flip()
+        pygame.time.Clock().tick(60)
+
+    else:
+        continue_button_rect, end_button_rect = draw_menu()
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if continue_button_rect.collidepoint(event.pos):
+                    speed = [4, 4]
+                    game_state = True
+                elif end_button_rect.collidepoint(event.pos):
+                    running = False
 
 pygame.quit()
